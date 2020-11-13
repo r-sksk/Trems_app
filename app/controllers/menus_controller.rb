34b4904a64ac2e39155942677e5f347e244e681f
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-11-13 19:09:20 +0900

    menusコントローラー記述

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,13 +1,54 @@
 class MenusController < ApplicationController
-  def index
-  end
+  before_action :authenticate_user!  #ログインしていないユーザーはログイン画面へ
 
-  def new
+  def index
+    @menus = Menu.all.page(params[:page]).per(12).search(params[:search]).order('created_at DESC')
   end
 
   def show
+    @menu = Menu.find(params[:id])
+  end
+
+  def new
+    @menu = Menu.new
   end
 
   def edit
+    @menu = Menu.find(params[:id])
+    if @menu.user != current_user
+      redirect_to menus_path, alert: '不正なアクセスです。'
+    end
   end
+
+  def create
+    @menu = Menu.new(menu_params)
+    @menu.user_id = current_user.id
+    if @menu.save
+      redirect_to menu_path(@menu), notice: '投稿に成功しました。'
+    else
+      render :new
+    end
+  end
+
+  def update
+    @menu = Menu.find(params[:id])
+    if @menu.update(menu_params)
+      redirect_to menu_path(@menu), notice: '更新に成功しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    menu = Menu.find(params[:id])
+    menu.destroy
+    redirect_to menus_path
+  end
+
+  private
+   def menu_params
+    params.require(:menu).permit(:title, :body, :image)
+   end
+
 end
+
