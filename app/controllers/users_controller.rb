dee65c8d7a4b68c4c8ed3ebc1348347258754e9c
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-11-13 19:07:42 +0900

    usersコントローラー記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,39 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: [:index]  #ログインしていないユーザーはindex画面しか閲覧できない→ログイン画面へ
+
   def index
+    @users = User.all.page(params[:page]).per(12).search(params[:search])
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def edit
+    @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to users_path, alert: '不正なアクセスです。'
+    end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: '更新に成功しました。'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+    redirect_to root_path, notice: 'アカウントを削除しました。'
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :emai, :profile, :profile_image)
   end
 end
+
