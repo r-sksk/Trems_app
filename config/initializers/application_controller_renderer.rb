54035422a5e20a87f8e182d7a2471183a369b9c6
DESKTOP-DRAMV4V\Ryo <r.sksk818@gmail.com>
2020-11-14 18:24:00 +0900

    refile設定、投稿確認

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'fd87276fa8efac324cbee5d2161e158c3da8d8ac6142524aba79bf01d07ea4a3542a599e3ab93e2109e62b0258271fda5808dfe6ed5b254f53ee802770372fc4'
